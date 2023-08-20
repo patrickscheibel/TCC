268680b80a9e3dd94da4c778da87712106a17adf
patrickscheibel <teste@TCC.myguest.virtualbox.org>
2023-08-20 19:29:43 -0300

    Ajuste nas opcoes de daltonismo

--- src/components/getPageFromFilters.js
+++ src/components/getPageFromFilters.js
@@ -9,19 +9,19 @@ import Pagina3_Login from '../pages/Pagina3/Login.js';
 const getPageFromFilters = (filtro1, filtro2, filtro3, filtro4) => {
   let paginaComponent = null;
 
-  if (filtro1 === 'idade_01_10' && filtro2 === 'grau_1' && filtro3 === 'tipo_1') {
+  if (filtro1 === 'idade_01_10' && filtro2 === 'grau_1' && filtro3 === 'normal') {
     if (filtro4 === 'dashboard') {
       paginaComponent = <Pagina1_Dashboard />;
     } else if (filtro4 === 'login') {
       paginaComponent = <Pagina1_Login />;
     }
-  } else if (filtro1 === 'idade_10_20' && filtro2 === 'grau_2' && filtro3 === 'tipo_2') {
+  } else if (filtro1 === 'idade_10_20' && filtro2 === 'grau_2' && filtro3 === 'protanopia') {
     if (filtro4 === 'dashboard') {
       paginaComponent = <Pagina2_Dashboard />;
     } else if (filtro4 === 'login') {
       paginaComponent = <Pagina2_Login />;
     }
-  } else if (filtro1 === 'idade_20_30' && filtro2 === 'grau_3' && filtro3 === 'tipo_3') {
+  } else if (filtro1 === 'idade_20_30' && filtro2 === 'grau_3' && filtro3 === 'tritanopia') {
     if (filtro4 === 'dashboard') {
       paginaComponent = <Pagina3_Dashboard />;
     } else if (filtro4 === 'login') {
--- src/components/optionsDaltonism.js
+++ src/components/optionsDaltonism.js
@@ -1,8 +1,8 @@
 const optionsDaltonism = [
-    { value: 'tipo_1', label: 'Tipo 1' },
-    { value: 'tipo_2', label: 'Tipo 2' },
-    { value: 'tipo_3', label: 'Tipo 3' },
-    { value: 'tipo_4', label: 'Tipo 4' },
+    { value: 'normal', label: 'Normal' },
+    { value: 'protanopia', label: 'Protanopia' },
+    { value: 'tritanopia', label: 'Tritanopia' },
+    { value: 'deuteranopia', label: 'Deuteranopia' },
   ];
   
 export default optionsDaltonism;
