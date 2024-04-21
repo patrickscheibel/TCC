def2d658942c8cfceb610672ada9ac5f86a35217
patrick <teste@TCC.myguest.virtualbox.org>
2024-04-21 00:31:58 -0300

    Configurado o https na aplicacao

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const fs = require('fs');
+const https = require('https');
+const cors = require('cors');
 
 // Servir arquivos estáticos da pasta 'build'
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(cors());
 
 // Rota para lidar com todas as outras solicitações e servir o arquivo HTML principal
 app.get('*', (req, res) => {
@@ -15,3 +19,8 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Servidor iniciado na porta ${port}`);
 });
+
+https.createServer({
+    cert: fs.readFileSync('src/SSL/code.crt'),
+    key: fs.readFileSync('src/SSL/code.key')
+}, app).listen(3001, ()=> console.log("Rodando em https"));
