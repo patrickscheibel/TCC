01351cdb269ecd5b4d1a1f420393b0b947b91fad
patrick <teste@TCC.myguest.virtualbox.org>
2024-04-24 02:56:01 -0300

    Ajustado a tela de register e o css dele

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,30 +2,31 @@ import React from 'react'
 import './Register.css'
 
 const Register = (props) => {
-  const selectedStyle = props.selectedStyle
+  const daltonismo = props.daltonismo
+  const problemaVisao = props.problemaVisao
 
   return (
-    <div className={`${selectedStyle}`}>
+    <div className='register'>
       <h1>Cadastro</h1>
-      <form>
-        <div className={`input-${selectedStyle}`}>
+      <form className={`register-${problemaVisao}`}>
+        <div className='input-register'>
           <label htmlFor="username">Nome</label>
           <input type="text" id="username" placeholder="Digitar o seu nome" required />
         </div>
-        <div className={`input-${selectedStyle}`}>
+        <div className='input-register'>
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" placeholder="Digitar o seu your email" required />
+          <input type="email" id="email" placeholder="Digitar o seu email" required />
         </div>
-        <div className={`input-${selectedStyle}`}>
+        <div className='input-register'>
           <label htmlFor="password">Senha</label>
           <input type="password" id="password" placeholder="Digitar a sua senha" required />
         </div>
         
-        <div className={`button-${selectedStyle}`}>
+        <div className={`button-register ${daltonismo}`}>
           <button type="submit" disabled>Cadastrar</button>
         </div>
-        <div className={`button-${selectedStyle}`}>
-          <button className={`voltar-${selectedStyle}`} disabled>Voltar</button>
+        <div className={`button-register ${daltonismo}`}>
+          <button className={`voltar-register ${daltonismo}`} disabled>Voltar</button>
         </div>
       </form>
     </div>
