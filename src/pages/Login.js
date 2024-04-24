2d07ad55f036c8d91c911d2968db0d68a327635e
patrick <teste@TCC.myguest.virtualbox.org>
2024-04-24 02:55:20 -0300

    Ajustado a tela de login e o css dele

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,25 +2,26 @@ import React from 'react'
 import './Login.css'
 
 const Login = (props) => {
-  const selectedStyle = props.selectedStyle
+  const daltonismo = props.daltonismo
+  const problemaVisao = props.problemaVisao
 
   return (
-    <div className={selectedStyle}>
+    <div className='login'>
       <h1>Login</h1>
-      <form>
-        <div className={`input-${selectedStyle}`}>
+      <form className={`login-${problemaVisao}`}>
+        <div className='input-login'>
           <label htmlFor="username">Nome</label>
           <input type="text" id="username" placeholder="Digitar o seu nome" required />
         </div>
-        <div className={`input-${selectedStyle}`}>
+        <div className='input-login'>
           <label htmlFor="password">Senha</label>
           <input type="password" id="password" placeholder="Digitar sua senha" required />
         </div>
-        <div className={`button-${selectedStyle}`}>
+        <div className={`button-login ${daltonismo}`}>
           <button type="submit" disabled>Entrar</button>
         </div>
-        <div className={`button-${selectedStyle}`}>
-          <button className={`cadastro-button-${selectedStyle}`} disabled>Cadastrar</button>
+        <div className={`button ${daltonismo}`}>
+          <button className={`cadastro-button-login ${daltonismo}`} disabled>Cadastrar</button>
         </div>
       </form>
     </div>
