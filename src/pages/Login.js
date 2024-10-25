f5043dad42a648044b25d7c60c0772bcf3b3acf1
teste <teste@TCC.myguest.virtualbox.org>
2024-10-25 19:09:27 -0300

    Ajustado a tela do login e o css

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,26 +4,31 @@ import './Login.css'
 const Login = (props) => {
   const daltonismo = props.daltonismo
   const problemaVisao = props.problemaVisao
+  const idade = props.idade
 
   return (
-    <div className='login'>
-      <h1>Login</h1>
-      <form className={`login-${problemaVisao}`}>
-        <div className='input-login'>
-          <label htmlFor="username">Nome</label>
-          <input type="text" id="username" placeholder="Digitar o seu nome" required />
+    <div className={`login-${idade}`}>
+      <div className={`login-${problemaVisao}`}>
+        <div className='login'>
+          <h1>Login</h1>
+          <form>
+            <div className='input-login'>
+              <label htmlFor="username">Nome</label>
+              <input type="text" id="username" placeholder="Digitar o seu nome" required />
+            </div>
+            <div className='input-login'>
+              <label htmlFor="password">Senha</label>
+              <input type="password" id="password" placeholder="Digitar sua senha" required />
+            </div>
+            <div className={`button-login ${daltonismo}`}>
+              <button type="submit" disabled>Entrar</button>
+            </div>
+            <div className={`button ${daltonismo}`}>
+              <button className={`cadastro-button-login ${daltonismo}`} disabled>Cadastrar</button>
+            </div>
+          </form>
         </div>
-        <div className='input-login'>
-          <label htmlFor="password">Senha</label>
-          <input type="password" id="password" placeholder="Digitar sua senha" required />
-        </div>
-        <div className={`button-login ${daltonismo}`}>
-          <button type="submit" disabled>Entrar</button>
-        </div>
-        <div className={`button ${daltonismo}`}>
-          <button className={`cadastro-button-login ${daltonismo}`} disabled>Cadastrar</button>
-        </div>
-      </form>
+      </div>
     </div>
   )
 }
