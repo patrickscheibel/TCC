fc39bb7e4c45688dbaef32fb2042d26949f1799c
teste <teste@TCC.myguest.virtualbox.org>
2024-10-26 00:27:52 -0300

    Ajuste na estrutura da tela de register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,38 +3,35 @@ import './Register.css'
 
 const Register = (props) => {
   const daltonismo = props.daltonismo
-  const problemaVisao = props.problemaVisao
-  const idade = props.idade
+  const tamanhoTexto = props.tamanhoTexto
 
   return (
-    <div className={`register-${idade}`}>
-      <div className={`register-${problemaVisao}`}>
-        <div className='register'>
-          <h1>Cadastro</h1>
-          <form>
-            <div className='input-register'>
-              <label htmlFor="username">Nome</label>
-              <input type="text" id="username" placeholder="Digitar o seu nome" required />
-            </div>
-            <div className='input-register'>
-              <label htmlFor="email">Email</label>
-              <input type="email" id="email" placeholder="Digitar o seu email" required />
-            </div>
-            <div className='input-register'>
-              <label htmlFor="password">Senha</label>
-              <input type="password" id="password" placeholder="Digitar a sua senha" required />
-            </div>
-            
-            <div className={`button-register ${daltonismo}`}>
-              <button type="submit" disabled>Cadastrar</button>
-            </div>
-            <div className={`button-register ${daltonismo}`}>
-              <button className={`voltar-register ${daltonismo}`} disabled>Voltar</button>
-            </div>
-          </form>
-        </div>
+    <div className={`register-${tamanhoTexto}`}>
+      <div className='register'>
+        <h1>Cadastro</h1>
+        <form>
+          <div className='input-register'>
+            <label htmlFor="username">Nome</label>
+            <input type="text" id="username" placeholder="Digitar o seu nome" required />
+          </div>
+          <div className='input-register'>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" placeholder="Digitar o seu email" required />
+          </div>
+          <div className='input-register'>
+            <label htmlFor="password">Senha</label>
+            <input type="password" id="password" placeholder="Digitar a sua senha" required />
+          </div>
+          
+          <div className={`button-register ${daltonismo}`}>
+            <button type="submit" disabled>Cadastrar</button>
+          </div>
+          <div className={`button-register ${daltonismo}`}>
+            <button className={`voltar-register ${daltonismo}`} disabled>Voltar</button>
+          </div>
+        </form>
       </div>
-    </div>  
+    </div>
   )
 }
 
