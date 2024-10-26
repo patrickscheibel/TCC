eefe3c5721c22b41b72e7a80416b972d2f1e9e48
teste <teste@TCC.myguest.virtualbox.org>
2024-10-26 00:27:39 -0300

    Ajuste na estrutura da tela de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,31 +3,28 @@ import './Login.css'
 
 const Login = (props) => {
   const daltonismo = props.daltonismo
-  const problemaVisao = props.problemaVisao
-  const idade = props.idade
+  const tamanhoTexto = props.tamanhoTexto
 
   return (
-    <div className={`login-${idade}`}>
-      <div className={`login-${problemaVisao}`}>
-        <div className='login'>
-          <h1>Login</h1>
-          <form>
-            <div className='input-login'>
-              <label htmlFor="username">Nome</label>
-              <input type="text" id="username" placeholder="Digitar o seu nome" required />
-            </div>
-            <div className='input-login'>
-              <label htmlFor="password">Senha</label>
-              <input type="password" id="password" placeholder="Digitar sua senha" required />
-            </div>
-            <div className={`button-login ${daltonismo}`}>
-              <button type="submit" disabled>Entrar</button>
-            </div>
-            <div className={`button ${daltonismo}`}>
-              <button className={`cadastro-button-login ${daltonismo}`} disabled>Cadastrar</button>
-            </div>
-          </form>
-        </div>
+    <div className={`login-${tamanhoTexto}`}>
+      <div className='login'>
+        <h1>Login</h1>
+        <form>
+          <div className='input-login'>
+            <label htmlFor="username">Nome</label>
+            <input type="text" id="username" placeholder="Digitar o seu nome" required />
+          </div>
+          <div className='input-login'>
+            <label htmlFor="password">Senha</label>
+            <input type="password" id="password" placeholder="Digitar sua senha" required />
+          </div>
+          <div className={`button-login ${daltonismo}`}>
+            <button type="submit" disabled>Entrar</button>
+          </div>
+          <div className={`button ${daltonismo}`}>
+            <button className={`cadastro-button-login ${daltonismo}`} disabled>Cadastrar</button>
+          </div>
+        </form>
       </div>
     </div>
   )
